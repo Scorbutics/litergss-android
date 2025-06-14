785a1710c919b92954eacb5af1e3a254c15c6128
Scorbutics <brice.bulgarelli@gmail.com>
2025-06-14 07:38:43 +0200

    fix(build): make the build working on Windows under docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-BASEDIR=$(pwd)
+BASEDIR=/$(pwd)
 docker run \
     -v $BASEDIR:/opt/current \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     scor/ruby-android-ndk:latest \
-    /bin/bash \
-    -c 'cmake --build .'
+    cmake --build .

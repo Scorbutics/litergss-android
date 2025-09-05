b5a8e0692ab8aebb0617cad2f32755310514381a
Scorbutics <brice.bulgarelli@gmail.com>
2025-09-05 18:44:04 +1000

    build: build into docker container

--- scripts/tools/clean-all-libs.sh
+++ scripts/tools/clean-all-libs.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 . "$DIR/constants.sh"
 
-ROOT_BUILD_DIR="$SOURCE_DIR/ruby-build"
+ROOT_BUILD_DIR="$SOURCE_DIR/build"
 
 for item in $(find "$ROOT_BUILD_DIR"/* -maxdepth 0 -type d -print); do
     cmake --build . --target "$(basename $item)_clean"

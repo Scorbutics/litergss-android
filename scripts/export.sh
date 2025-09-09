1bf00f630698f76e4dbdcb4a5159dc5c672029cf
Scorbutics <brice.bulgarelli@gmail.com>
2025-09-09 22:32:04 +1000

    fix: adapt parts of code to avoid arch conflicts

--- scripts/export.sh
+++ scripts/export.sh
@@ -3,5 +3,5 @@
 # ex: ~/dev/PSDK-android
 HOME_ANDROID_PROJECT="$1"
 cp -rf target/android/assets/* "$HOME_ANDROID_PROJECT/app/src/main/assets/"
-cp -rf target/android/ext-native-libs/include/* "$HOME_ANDROID_PROJECT/app/src/main/cpp/external/"
-cp -rf target/android/jniLibs/* "$HOME_ANDROID_PROJECT/app/src/main/jniLibs/"
+cp -rf target/android/include/* "$HOME_ANDROID_PROJECT/app/src/main/cpp/external/"
+cp -rf target/android/bin/* "$HOME_ANDROID_PROJECT/app/src/main/jniLibs/"

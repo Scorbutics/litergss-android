c8f4c23c7e01d26bba1b7e7cb91ece2624b2cfe9
Scorbutics <brice.bulgarelli@gmail.com>
2025-09-09 22:50:41 +1000

    fix: adapt parts of code to PAPK

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,6 +2,7 @@
 
 # ex: ~/dev/PSDK-android
 HOME_ANDROID_PROJECT="$1"
-cp -rf target/android/assets/* "$HOME_ANDROID_PROJECT/app/src/main/assets/"
-cp -rf target/android/include/* "$HOME_ANDROID_PROJECT/app/src/main/cpp/external/"
-cp -rf target/android/bin/* "$HOME_ANDROID_PROJECT/app/src/main/jniLibs/"
+
+cp -rf target/android/assets/* "$HOME_ANDROID_PROJECT/plugins/ruby-vm/shared/src/main/cpp/assets/files/"
+cp -rf target/android/include/* "$HOME_ANDROID_PROJECT/plugins/ruby-vm/shared/src/main/cpp/external/include"
+cp -rf target/android/lib/* "$HOME_ANDROID_PROJECT/plugins/ruby-vm/shared/src/main/cpp/external/lib"

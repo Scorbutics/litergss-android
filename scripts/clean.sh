9718bf033c8ec035a674444fe5cad8666e8cf68c
Scorbutics <brice.bulgarelli@gmail.com>
2025-09-14 09:51:11 +1000

    fix: adapt parts for easier support of multi arch

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,11 +2,18 @@
 
 TARGETS="$@"
 DIR="$(cd "$(dirname "$0")" && pwd)"
+DELIMITER='_'
 
 if [ -z "$TARGETS" ]; then
-  "$DIR/tools/docker-dev-action.sh" cmake --build . --target clean-all
+  "$DIR/tools/docker-dev-action.sh" cmake --build . --target clean-libs
 else
   for target in $TARGETS; do
-    "$DIR/tools/docker-dev-action.sh" cmake --build . --target "${target}_clean"
+    last_field=$(echo "$target" | awk -F "$DELIMITER" 'NF > 1 {print $NF}')
+    if [ -n "$last_field" ]; then
+      first_field=$(echo "$target" | cut -d "$DELIMITER" -f 1) 
+      "$DIR/tools/docker-dev-action.sh" cmake --build . --target "${first_field}_clean_${last_field}"
+    else
+      "$DIR/tools/docker-dev-action.sh" cmake --build . --target "${target}_clean"
+    fi
   done
 fi;
